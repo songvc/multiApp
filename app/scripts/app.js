73948d7c28d327765ee379d7f0fa951ad2e3caab
Vincent Song <songvc@gmail.com>
2015-04-07 18:19:26 -0700

    v1

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name multiAppApp
+ * @description
+ * # multiAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('multiAppApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name multiAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the multiAppApp
+ */
+angular.module('multiAppApp')
+  .controller('MultiplicationCtrl', function ($scope) {
+    $scope.numbers = [1,2,3,4,5,6,7,8,9,10];
+  });
